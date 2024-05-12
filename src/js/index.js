fe58302f6a2494ab4f9e6a0e68ade32685d6d568
cristinagher <cristinagamezhernandez@gmail.com>
2024-05-12 19:07:44 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+let next = document.querySelector('.next');
+let prev = document.querySelector('prev');
+
+addEventListener('click', function() {
+    let items = document.querySelectorAll ('.slide-item')
+    document.querySelector('.slide').appendChild(items[0])
+} )
